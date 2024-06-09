cb72760849e4aee1e8f0bf4c77b975e736b0f6fc
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-10 00:01:56 +0200

    Route Params: id param from routes is used in the VanDetail file to console log the id of whatever van is clicked on

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans"
+import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 
 import "./server";
 
@@ -22,11 +23,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
 
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
