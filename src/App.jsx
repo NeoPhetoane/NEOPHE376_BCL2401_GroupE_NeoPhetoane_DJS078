a5da16ba2dda6063062e1136f5d2d2640971b272
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-09 23:43:42 +0200

    Vans page displayed and a detail page created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans"
 
 import "./server";
 
@@ -21,9 +22,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
 }
 export default App;
+
+
