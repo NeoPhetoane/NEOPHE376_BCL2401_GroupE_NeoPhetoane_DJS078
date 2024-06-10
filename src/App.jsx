4157d150e1695475bbae306eac13c38d0e1dc994
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-10 10:53:40 +0200

    Vans nav component added to HostLayout and linked to its own page called HostVans and HostVanDetail

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import VanDetail from "./pages/Vans/VanDetail";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
+import HostVanDetail from "./pages/Host/HostVanDetail";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
 
@@ -16,17 +18,19 @@ import "./server";
 function App() {
   return (
     <BrowserRouter>
-       <Routes>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          
+
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
