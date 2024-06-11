734805638d8920dcffbe2d6ad1cd68f6520d27ca
NeoPhetoane <phetoaneneo4@gmail.com>
2024-06-11 11:29:37 +0200

    Added filtering of vans by type based on search parameters.

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -2,21 +2,22 @@ import React from "react"
 import { Link, useSearchParams } from "react-router-dom"
 
 export default function Vans() {
-    
     const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
     
     const typeFilter = searchParams.get("type")
-    console.log(typeFilter)
-    
     
     React.useEffect(() => {
         fetch("/api/vans")
             .then(res => res.json())
             .then(data => setVans(data.vans))
     }, [])
+    
+    const displayedVans = typeFilter
+        ? vans.filter(van => van.type === typeFilter)
+        : vans
 
-    const vanElements = vans.map(van => (
+    const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} />
